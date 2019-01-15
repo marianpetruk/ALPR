44fe8a2abfd18bdf43e6342ed4d409e345315780
AntonBorkivskyy <anton.borkivskyi@gmail.com>
2019-01-15 12:32:17 +0200

    Add image segmenting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from detection.detect import detect
+from segmentation.plate_recognition.recognize.recognize import segmenting
 import cv2
 
 
@@ -6,6 +7,9 @@ def main():
     test_img = "detection/data/1.jpg"
 
     img = detect(testimg=test_img, model="detection/checkpoint/model_best.pth.tar")
+
+    chars = segmenting(img)
+
 
     # cv2.imshow("image", img)
     # cv2.waitKey(0)
